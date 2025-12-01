9c53afcd873962c41e439d2db58cb25ec3844891
Elisson Silva <nossilesilva@gmail.com>
2025-12-01 12:58:59 -0300

    feat: added admin

--- src/infra/database/kysely/user-repository-kysely.ts
+++ src/infra/database/kysely/user-repository-kysely.ts
@@ -1,8 +1,14 @@
 import User from "@/domain/entities/user";
-import UserRepository, { CreateUserDTO } from "@/domain/repositories/user-repository";
+import UserRepository, { CreateUserDTO, ListUsersFilters, PaginatedResult } from "@/domain/repositories/user-repository";
 
 // comunica com o banco de dados
 export class UserRepositoryKysely implements UserRepository {
+  async deleteByExternalId(externalId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  async list(page: number, limit: number, filters?: ListUsersFilters): Promise<PaginatedResult<User>> {
+    throw new Error("Method not implemented.");
+  }
   async findById(id: number): Promise<User | null> {
     throw new Error("Method not implemented.");
   }
